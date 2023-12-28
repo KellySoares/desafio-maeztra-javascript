88a368f1fe34ec3e2017d05e8e0f2effa357f607
Kelly Soares <kellyps_2012@yahoo.com.br>
2023-12-28 15:50:57 -0300

    resposta da questão 5

--- resposta_questao_5.js
+++ resposta_questao_5.js
@@ -1,1 +1,27 @@
 
+//Definição inicial de valores em um array
+const inputs = [2,3, 5, 9, 10, 4, 6, 7]
+function VerificaPossibilidade({ qddPessoas }) {
+    //Inicia a variavel poses com 1
+    let poses = 1
+   //Itera sobre a quantidade de pessoas decresmentando um enquanto for maior que 1
+    while(qddPessoas > 1){
+        //Calcula a quantidade de poses utilizando a permutação simples, que seria n!(n = Quantidade de pessoas), 
+        poses = qddPessoas * poses
+        qddPessoas--
+    }
+
+    return poses
+
+}
+//Iteração do Array de valores iniciais
+inputs.forEach(qddPessoas => {
+    //Acesso da funcao com cada valor do array, retornando um result como resposta
+    const result = VerificaPossibilidade({
+        qddPessoas: qddPessoas
+    })
+    //Print no console do valor de entrada e resultado da função
+    console.log(`Em um grupo de ${qddPessoas} amigos, eles podem se organizar ${result} maneiras distintas`)
+})
+
+
