441e4f63e229d089be2fc99de24be6ce77765faf
Kelly Soares <kellyps_2012@yahoo.com.br>
2024-01-03 13:32:02 -0300

    Ajustes de rotas de imagens

--- layout/src/components/store/reducers/footerContent.js
+++ layout/src/components/store/reducers/footerContent.js
@@ -2,54 +2,54 @@ const footerList = [
   {
     'network': [
       {
-        image: "./desafio-maeztra-javascript/images/footer/facebook.svg",
-        title: "Facebook",
-        link: "https://www.facebook.com"
+        image: `${process.env.PUBLIC_URL}/images/footer/facebook.svg`,
+        title: `Facebook`,
+        link: `https://www.facebook.com`
       },
       {
-        image: "./desafio-maeztra-javascript/images/footer/linkedln.svg",
-        title: "Linkedln",
-        link: "https://www.linkedin.com"
+        image:  `${process.env.PUBLIC_URL}/images/footer/linkedln.svg`,
+        title:  `Linkedln`,
+        link:  `https://www.linkedin.com`
       },
       {
-        image: "./desafio-maeztra-javascript/images/footer/instagram.svg",
-        title: "Instagram",
-        link: "https://www.instagram.com"
+        image:  `${process.env.PUBLIC_URL}/images/footer/instagram.svg`,
+        title:  `Instagram`,
+        link:  `https://www.instagram.com`
       },
       {
-        image: "./desafio-maeztra-javascript/images/footer/youtube.svg",
-        title: "YouTube",
-        link: "https://www.youtube.com"
+        image:  `${process.env.PUBLIC_URL}/images/footer/youtube.svg`,
+        title:  `YouTube`,
+        link:  `https://www.youtube.com`
       }
     ],
     'payment': [
       {
-        image: "./desafio-maeztra-javascript/images/footer/visa.svg",
-        title: "Visa"
+        image:  `${process.env.PUBLIC_URL}/images/footer/visa.svg`,
+        title:  `Visa`
       },
       {
-        image: "./desafio-maeztra-javascript/images/footer/master.svg",
-        title: "Visa"
+        image:  `${process.env.PUBLIC_URL}/images/footer/master.svg`,
+        title:  `Visa`
       },
       {
-        image: "./desafio-maeztra-javascript/images/footer/visa.svg",
-        title: "Visa"
+        image:  `${process.env.PUBLIC_URL}/images/footer/visa.svg`,
+        title:  `Visa`
       },
       {
-        image: "./desafio-maeztra-javascript/images/footer/master.svg",
-        title: "Visa"
+        image:  `${process.env.PUBLIC_URL}/images/footer/master.svg`,
+        title:  `Visa`
       }
     ],
     'by': [
       {
         text: 'Powered by',
-        image: "./desafio-maeztra-javascript/images/footer/logo-footer.svg",
-        title: "Vtex"
+        image:  `${process.env.PUBLIC_URL}/images/footer/logo-footer.svg`,
+        title:  `Vtex`
       },
       {
         text: 'Developed by',
-        image: "./desafio-maeztra-javascript/images/footer/maeztra-logo.svg",
-        title: "Maeztra"
+        image:  `${process.env.PUBLIC_URL}/images/footer/maeztra-logo.svg`,
+        title:  `Maeztra`
       }
     ]
   },
--- layout/src/components/store/reducers/fullBanner.js
+++ layout/src/components/store/reducers/fullBanner.js
@@ -1,32 +1,32 @@
 const bannersList = [{
-  image: "./desafio-maeztra-javascript/images/banner/fullbanner.png",
-  title: "Promoções de Outono",
-  description: "Confiras os melhores looks para combinar com você nesse Outono"
+  image: `${process.env.PUBLIC_URL}/images/banner/fullbanner.png`,
+  title: `Promoções de Outono`,
+  description: `Confiras os melhores looks para combinar com você nesse Outono`
 },
 {
-  image: "./desafio-maeztra-javascript/images/banner/fullbanner.png",
-  title: "Promoções de Outono",
-  description: "Confiras os melhores looks para combinar com você nesse Outono"
+  image: `${process.env.PUBLIC_URL}/images/banner/fullbanner.png`,
+  title: `Promoções de Outono`,
+  description: `Confiras os melhores looks para combinar com você nesse Outono`
 },
 {
-  image: "./desafio-maeztra-javascript/images/banner/fullbanner.png",
-  title: "Promoções de Outono",
-  description: "Confiras os melhores looks para combinar com você nesse Outono"
+  image: `${process.env.PUBLIC_URL}/images/banner/fullbanner.png`,
+  title: `Promoções de Outono`,
+  description: `Confiras os melhores looks para combinar com você nesse Outono`
 },
 {
-  image: "./desafio-maeztra-javascript/images/banner/fullbanner.png",
-  title: "Promoções de Outono",
-  description: "Confiras os melhores looks para combinar com você nesse Outono"
+  image: `${process.env.PUBLIC_URL}/images/banner/fullbanner.png`,
+  title: `Promoções de Outono`,
+  description: `Confiras os melhores looks para combinar com você nesse Outono`
 },
 {
-  image: "./desafio-maeztra-javascript/images/banner/fullbanner.png",
-  title: "Promoções de Outono",
-  description: "Confiras os melhores looks para combinar com você nesse Outono"
+  image: `${process.env.PUBLIC_URL}/images/banner/fullbanner.png`,
+  title: `Promoções de Outono`,
+  description: `Confiras os melhores looks para combinar com você nesse Outono`
 },
 {
-  image: "./desafio-maeztra-javascript/images/banner/fullbanner.png",
-  title: "Promoções de Outono",
-  description: "Confiras os melhores looks para combinar com você nesse Outono"
+  image: `${process.env.PUBLIC_URL}/images/banner/fullbanner.png`,
+  title: `Promoções de Outono`,
+  description: `Confiras os melhores looks para combinar com você nesse Outono`
 }];
 
 export default bannersList;
