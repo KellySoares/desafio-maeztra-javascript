f0164120c49abb84c6312d49196eb6f0b08a0214
Kelly Soares <kellyps_2012@yahoo.com.br>
2024-01-05 09:57:45 -0300

    Finalização do modal

--- layout/src/components/modal/index.js
+++ layout/src/components/modal/index.js
@@ -3,10 +3,38 @@ import Backdrop from '@mui/material/Backdrop';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import Fade from '@mui/material/Fade';
-import Button from '@mui/material/Button';
+import { Button, TextField } from "@mui/material";
 import './index.scss';
 
 import Img from "../../assets/images/image 4.png";
+import iconEnviar from "../../assets/images/icons/icon_enviar.svg";
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+
+import { styled } from '@mui/material/styles';
+const CssTextField = styled(TextField)({
+    '& label.Mui-focused': {
+        color: '#FAA500',
+    },
+    '& .MuiInput-underline:after': {
+        borderColor: '#353535',
+    },
+    '& .MuiOutlinedInput-root': {
+        lineHeight: '18px',
+        borderRadius: '4px',
+        paddingRight: '0px',
+        backgroundColor: '#FFFFFF',
+        fontSize: '12px',
+        '& fieldset': {
+            borderColor: '#353535',
+        },
+        '&:hover fieldset': {
+            borderColor: '#FAA500',
+        },
+        '&.Mui-focused fieldset': {
+            borderColor: '#FAA500',
+        },
+    },
+});
 
 const styleClose = {
     position: 'absolute',
@@ -14,7 +42,7 @@ const styleClose = {
     width: '100%',
     bgcolor: 'transparent',
     right: '0',
-    top: '-20px',
+    top: '-20px'
 
 };
 const styleButton = {
@@ -22,14 +50,15 @@ const styleButton = {
     color: '#FFFFFF',
     fontFamily: 'Lato !important',
     fontSize: '12px !important',
-    minWidth: '0px'
+    minWidth: '0px',
+    fontWeight: 400
 
 };
 
 const ModalComp = () => {
 
     const [open, setOpen] = React.useState(true);
-    const handleClose = () => setOpen(false);
+    const handleClose = () => setOpen(true);
 
     return (
         <div>
@@ -48,18 +77,46 @@ const ModalComp = () => {
             >
                 <Fade in={open}>
 
-                    <Box className='modal' sx={{width:  { xs: '80%', md: '50%' }}}>
+                    <Box className='modal' sx={{ width: { xs: '80%', md: '60%' } }}>
                         <Box sx={styleClose} className='modal-close'>
                             <Button sx={styleButton} autoFocus onClick={handleClose} className='modal-close_button'>
                                 Fechar
                             </Button>
                         </Box>
-                        <Box key='modal' sx={{ gap: 3, display: 'flex', flexDirection: 'row', alignItems: 'center', backgroundColor: '#EFEFEF', width:  { xs: '100%', md: '50%' } }} className='modal-content' >
+                        <Box key='modal' sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', backgroundColor: '#FFFFFF', width: '100%' }} className='modal-content' >
                             <img src={Img} alt='' className='modal-content_image' />
-                            <Box sx={{ display: 'flex', flexDirection: 'column', textAlign: 'left' }} className='modal-content_text' >
-                                <p className='tipbar-box-text'>Bem Vindo à MAEZTRA</p>
-                                <p className='tipbar-box-description'>Receba em Primeira mão
-                                    desconto e ofertas exclusivas</p>
+                            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center', padding: { xs: '2em 2em', md: '4em 1em' } }} className='modal-content_text' >
+                                <MailOutlineIcon />
+                                <p className='text'>Bem Vindo à MAEZTRA</p>
+                                <p className='description'>Receba em Primeira mão</p>
+                                <p className='description_strong'> desconto e ofertas exclusivas</p>
+
+                                <form className="form">
+                                    <CssTextField
+                                        placeholder="Digite seu e-mail"
+                                        size="small"
+                                        sx={{
+                                            display: 'flex',
+                                            width: '100%',
+                                            color: '#787D83',
+                                            justifyContent: 'center',
+                                            backgroundColor: '#FFFFFF',
+                                            borderRadius: '5px',
+                                            border: '1px solid #C4C4C4',
+                                            marginTop: '1em'
+                                        }}
+                                        className="form-input" />
+                                    <Button
+                                        variant="contained"
+                                        sx={{
+
+                                        }}
+                                        onClick={handleClose}
+                                        className="form-button"
+                                        endIcon={<img src={iconEnviar} alt='' />}
+                                        disableElevation
+                                    >Enviar</Button>
+                                </form>
                             </Box>
                         </Box>
                     </Box>
